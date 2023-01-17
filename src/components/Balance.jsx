973ff18f854d7de15648407607157044063446d8
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-01-17 20:09:49 +0800

    computation

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ExpenseContext } from '../context/ExpenseContext';
 
 const Balance = () => {
+
+  const { transactions } = useContext(ExpenseContext);
+  const balance = transactions.map(transaction => transaction.amount).reduce((a, b) => a + b, 0);
+  const currency = new Intl.NumberFormat("en-us", {
+    currency: "USD",
+    style: "currency",
+  })
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>{currency.format(balance)}</h1>
     </>
   )
 }
--- src/components/IncomeExpenses.jsx
+++ src/components/IncomeExpenses.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { useContext } from 'react';
+import { ExpenseContext } from '../context/ExpenseContext';
 
 const IncomeExpenses = () => {
+  const { transactions } = useContext(ExpenseContext);
+  const toCurrency = new Intl.NumberFormat("en-us", {
+    currency: 'USD',
+    style: 'currency'
+  })
+
+  const income = transactions.filter(item => item.amount > 0).map(item => item.amount).reduce((a, b) => a + b, 0);
+  const expense = transactions.filter(item => item.amount < 0).map(item => item.amount).reduce((a, b) => a + b, 0);
+
   return (
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
-        <p className='money plus'>+$0.00</p>
+        <p className='money plus'>
+          +{toCurrency.format(income)}
+        </p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className='money minus'>-$0.00</p>
+        <p className='money minus'>
+          {toCurrency.format(expense)}
+        </p>
       </div>
     </div>
   )
