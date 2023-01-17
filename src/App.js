39fdef8c0843314ef203432c43a1b0fdfe3ba94f
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-01-17 15:52:22 +0800

    context provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Header from './components/Header';
-
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import { ExpenseProvider } from './context/ExpenseContext';
 
 function App() {
   return (
-    <div>
+    <ExpenseProvider>
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </ExpenseProvider>
   );
 }
 
