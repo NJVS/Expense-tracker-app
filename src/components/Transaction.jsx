e64894dde57483e6bd535b2faf65d5a79cc7114a
NJVS <neiljonathanv.sagisi+github@gmail.com>
2023-01-17 20:11:38 +0800

    display transactions

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Transaction = ({ data }) => {
-  const sign = data.amount < 0 ? '-' : '+';
+
+  const toCurrency = new Intl.NumberFormat("en-us", {
+    currency: "USD",
+    style: "currency",
+  })
 
   return (
-    <li className={sign === '-' ? 'minus' : 'plus'}>
-      {data.title} <span>{sign}${Math.abs(data.amount)}</span><button className='delete-btn'>x</button>
+    <li className={data.amount < 0 ? 'minus' : 'plus'}>
+      {data.title} <span>{toCurrency.format(data.amount)}</span><button className='delete-btn'>x</button>
     </li>
   )
 }
